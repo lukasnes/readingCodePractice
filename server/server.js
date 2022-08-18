db9435598212f0ba2d546f82c2c11632553ed4f2
Lukas <lukasnes.work@gmail.com>
2022-08-18 15:03:52 -0500

    Fixed port

--- server/server.js
+++ server/server.js
@@ -8,4 +8,6 @@ app.use(express.static('client'))
 
 require('./router/router.js')(app)
 
-app.listen(4000,console.log("Server running on 4000"))
+const port = process.env.PORT || 4000
+
+app.listen(port,console.log("Server running on 4000"))
